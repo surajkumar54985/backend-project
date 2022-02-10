378cbea779643fab588f5e2907aca6a66a8ac28d
Suraj kumar <surajkumar54985@gmail.com>
2022-02-10 15:13:03 +0530

    1st push of backend project on github using git

---
+++ controllers/home_controller.js
@@ -0,0 +1,53 @@
+const Post = require("../models/post");
+
+const User = require('../models/user');
+
+// module.exports.home = function(req,res){
+//     //console.log(req.cookies);
+//     //res.cookie('user_id' , 25);
+//     return res.render('home' , {
+//         title :"codial | Home",
+//     });
+// }
+
+/*module.exports.home = function(req,res){
+    User.find({})
+    .populate('user')
+    .exec(function(err , users){
+
+        User.find({} , function(err,users){
+            return res.render('home' , {
+                title: "codial | Home",
+                all_users: users
+            });
+        });
+        
+    })
+}*/
+
+module.exports.home = async function(req,res){
+    try{
+        //populate the user of each post
+        let posts = await Post.find({})
+        .sort('_createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+        let users = await User.find({}).populate('user');
+
+        return res.render('home', {
+            title: "codial|home",
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+}
+
+//module.exports.actionName = function(){}
